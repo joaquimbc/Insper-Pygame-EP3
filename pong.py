45c0fdb3eb2cae80c3ffe6db8c60770f5a217944
joaquimbc <joaquimbc@al.insper.edu.br>
2023-05-20 23:10:16 -0300

    Primeiro commit. Esqueleto do jogo

---
+++ pong.py
@@ -0,0 +1,32 @@
+import sys
+import pygame
+from random import randint
+
+# Constantes do jogo
+LARGURA, ALTURA = 800, 600
+RAIO_BOLA = 10
+LARGURA_RAQUETE, ALTURA_RAQUETE = 15, 80
+FPS = 60
+TAMANHO_FONTE = 50
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+class Game:
+    def __init__(self, win, clock, font, tipo):
+        pass
+
+class Player:
+    def __init__(self, name, tipo):
+        pass
+
+class Menu:
+    def __init__(self, win, clock, font):
+        pass
+
+def main():
+    print("Ola!")
+
+if __name__ == "__main__":
+    main()
