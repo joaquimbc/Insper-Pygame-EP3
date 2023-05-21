5423992ef8cb7ead3b9c2f2e42dc0a6cef96f9c2
Enrico2012 <joaquimbc@al.insper.edu.br>
2023-05-20 23:11:57 -0300

    Implementa a função renderiza_tela()

--- pong.py
+++ pong.py
@@ -23,6 +23,30 @@ class Player:
 
 class Menu:
     def __init__(self, win, clock, font):
+        self.win = win
+        self.clock = clock
+        self.font = font
+
+    def renderiza_tela(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_g: # Modo principal do jogo
+                        pass
+                    elif event.key == pygame.K_c: # Placar (Não implementado)
+                        pass 
+                    elif event.key == pygame.K_m: # Multiplayer (Não implementado)
+                        pass 
+
+            self.win.fill((0, 0, 0))
+            self.menu_principal()
+            pygame.display.flip()
+            self.clock.tick(FPS)
+    
+    def menu_principal(self):
         pass
 
 def main():
